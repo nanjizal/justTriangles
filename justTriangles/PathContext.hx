cd57b9bf385b7613aea828b2ef9d65d3cba0d10d
nanjizal <nitsuj@nanjizal.net>
2017-01-04 06:27:02 +0000

    Added PathContext for simple drawing API
    Need to add code to 'triangles' to allow ends to be nicely rounded, extract from isolated line or similar.

---
+++ justTriangles/PathContext.hx
@@ -0,0 +1,50 @@
+package justTriangles;
+import justTriangles.Draw;
+import justTriangles.Point;
+import justTriangles.ShapePoints;
+class PathContext {
+    var p0: Point;
+    var pp: Array;
+    var ppp: Array = Array<Array<Point>>();
+    public var id: Int;
+    public function new( id_: Int ){
+        id = id_;
+        moveTo( 0, 0 );
+    }
+    public function moveTo( x: Float, y: Float ): Void {
+        p0 = { x: x, y: y };
+        pp = new Array();
+        pp.push( p0 );
+        ppp.push( pp );
+    }
+    public function lineTo( x: Float, y: Float ): Void {
+        var p1: Point = { x: x, y: y };
+        pp.push( p1 );
+        p0 = p1;
+    }
+    public function quadTo( x1: Float, y1: Float, x2: Float, y2: Float ): Void {
+        var p1: Point = { x: x1, y: y1 };
+        var p2: Point = { x: x2, y: y2 };
+        ShapePoints.quadCurve( p0, p1, p2 );
+        p0 = p2;
+    }
+    public function curveTo( x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float ): Void {
+        var p1: Point = { x: x1, y: y1 };
+        var p2: Point = { x: x2, y: y2 };
+        var p3: Point = { x: x2, y: y3 };
+        ShapePoints.cubicCurve( p0, p1, p2, p3 );
+        p0 = p3;
+    }
+    public function render( thick: Float, ?outline: Bool = true ){
+        for( pp0 in ppp ){
+            Draw.triangles( id, outline, pp0 );
+        }
+    }
+    public function clear(){
+        for( pp0 in ppp ){
+            pp0.length = 0;
+        }
+        ppp.length = 0;
+        p0 = null;
+    }
+}
