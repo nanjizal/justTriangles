29674a55e7311287f2efc89e2a3c8ef496ba67d2
nanjizal <nitsuj@nanjizal.net>
2017-01-07 18:48:30 +0000

    PathContext easier scales, thickness broken?

--- justTriangles/PathContext.hx
+++ justTriangles/PathContext.hx
@@ -6,35 +6,48 @@ class PathContext {
     var p0: Point;
     var pp: Array<Point>;
     var ppp: Array<Array<Point>>;
+    var ppp_: Array<Array<Point>>;
+    var s: Float;
+    var dw: Float;
+    var tx: Float;
+    var ty: Float;
     public var id: Int;
-    public function new( id_: Int ){
+    public function new( id_: Int, width_: Float, ?tx_: Float = 0, ?ty_: Float = 0){
         id = id_;
+        dw = width_/2;
+        s = 1/width_;
+        tx = tx_;
+        ty = ty_;
         ppp = new Array<Array<Point>>();
-        moveTo( 0, 0 );
+        moveTo( dw, dw );
+    }
+    inline function pt( x: Float, y: Float ): Point {
+        // default is between ±1
+        return { x: s*( x - dw + tx ), y: s*( y - dw + ty ) }
     }
     public function moveTo( x: Float, y: Float ): Void {
-        p0 = { x: x, y: y };
+        p0 = pt( x, y );
         pp = new Array();
         pp.push( p0 );
         ppp.push( pp );
     }
     public function lineTo( x: Float, y: Float ): Void {
-        var p1: Point = { x: x, y: y };
+        var p1: Point = pt( x, y );
         pp.push( p1 );
         p0 = p1;
     }
     public function quadTo( x1: Float, y1: Float, x2: Float, y2: Float ): Void {
-        var p1: Point = { x: x1, y: y1 };
-        var p2: Point = { x: x2, y: y2 };
+        var p1: Point = pt( x1, y1 );
+        var p2: Point = pt( x2, y2 );
         var pMore: Array<Point> = ShapePoints.quadCurve( p0, p1, p2 );
         var plen = pp.length;
         for( i in 1...( pMore.length ) ) pp[ plen++ ] = pMore[ i ];
         p0 = p2;
     }
     public function curveTo( x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float ): Void {
-        var p1: Point = { x: x1, y: y1 };
-        var p2: Point = { x: x2, y: y2 };
-        var p3: Point = { x: x2, y: y3 };
+        var p1: Point = pt( x1, y1 );
+        var p2: Point = pt( x2, y2 );
+        var p3: Point = pt( x3, y3 );
         var pMore = ShapePoints.cubicCurve( p0, p1, p2, p3 );
         var plen = pp.length;
         for( i in 1...( pMore.length ) ) pp[ plen++ ] = pMore[ i ];
@@ -47,8 +60,8 @@ class PathContext {
         }
     }
     inline function reverseEntries(){
+        var p: Array<Point>;
         if( ppp_ == null ){
-            var p: Array<Point>;            
             ppp_ = new Array<Array<Point>>();
             for( pp0 in ppp ){
                 if( pp0.length > 1 ) {
@@ -60,6 +73,7 @@ class PathContext {
         }
     }
     public function clear(){
+        ppp_ = null;
         ppp = null;
         pp = null;
         p0 = null;
