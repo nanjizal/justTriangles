df4a9e9ed17f3cde659743763baa8060ab6a7de4
nanjizal <nitsuj@nanjizal.net>
2017-01-08 16:13:37 +0000

    Improving PathContext

--- justTriangles/ShapePoints.hx
+++ justTriangles/ShapePoints.hx
@@ -3,6 +3,18 @@ import justTriangles.Point;
 import justTriangles.Bezier;
 class ShapePoints {
     // Create Rectangular Box Points
+    public static inline function boxPoints( p: Point, wid: Float, hi: Float ): Array<Point>{
+        var p: Array<Point> = [     { x: p.x, y: p.y }
+                                ,   { x: p.x+wid, y: p.y }
+                                ,   { x: p.x+wid, y: p.y+hi }
+                                ,   { x: p.x, y: p.y+hi }
+                                ,   { x: p.x, y: p.y }
+                                ,   { x: p.x+wid, y: p.y }
+                                ,   { x: p.x+wid, y: p.y+hi }
+                                ];
+        return p;
+    }
+    // Create Rectangular Box Points
     public static inline function box( x: Float,y: Float, wid: Float, hi: Float ): Array<Point>{
         var p: Array<Point> = [     { x: x, y: y }
                                 ,   { x: x+wid, y: y }
@@ -26,6 +38,20 @@ class ShapePoints {
             p.push( { x: dx + radius * Math.cos( angle ), y: dy + radius * Math.sin( angle ) });
         } 
         p.reverse();
+        return p;
+    }
+    // Create Polygon Points
+    public static inline function polyPoints( d: Point, radius: Float, sides: Int, ?rotation: Float = 0 ):Array<Point>{
+        var p: Array<Point> = new Array<Point>();
+        var angle: Float = 0;
+        var angleInc: Float = ( Math.PI*2 )/sides;
+        var offset: Float = rotation - Math.PI/2;
+        for( i in 0...( sides + 3 ) ){
+            angle = i*angleInc;
+            angle = angle + offset; // ?  to test!
+            p.push( { x: d.x + radius * Math.cos( angle ), y: d.y + radius * Math.sin( angle ) });
+        } 
+        //p.reverse();
         return p;
     }
     // Create Polygon Points
@@ -59,6 +85,36 @@ class ShapePoints {
         var angleInc: Float = ( Math.PI*2 )/sides;
         var len: Int = Std.int( sides*repeats );
         for( i in 0...len ) p.push( { y: y_ + (dy+=dy_), x: x_ + amplitude * Math.sin( i*angleInc ) });
+        return p;
+    }
+    // Create Arc Points
+    public static inline function arcPoints( d: Point, radius: Float, start: Float, dA: Float, sides: Int ):Array<Point>{
+        var p: Array<Point> = new Array<Point>();
+        var dx = d.x;
+        var dy = d.y;
+        var angle: Float = 0;
+        var angleInc: Float = ( Math.PI*2 )/sides;
+        var sides = Math.round( sides );
+        var nextAngle: Float;
+        if( dA < 0 ){
+            var i = -1;
+            while( true ){
+                angle = i*angleInc;
+                nextAngle = angle + start; 
+                i--;
+                if( angle <= dA ) break; 
+                p.push( { x: dx + radius * Math.cos( nextAngle ), y: dy + radius * Math.sin( nextAngle ) });
+            } 
+        } else {
+            var i = -1;
+            while( true ){
+                angle = i*angleInc;
+                i++;
+                nextAngle = angle + start; 
+                if( angle >= ( dA + angleInc ) ) break; 
+                p.push( { x: dx + radius * Math.cos( nextAngle ), y: dy + radius * Math.sin( nextAngle ) });
+            } 
+        }
         return p;
     }
     // Create Arc Points
@@ -128,18 +184,16 @@ class ShapePoints {
         var step = Math.min( 1/( approxDistance*0.707 ), cubicStep );
         var arr = [ p0, p1, p2, p3 ];
         var t = 0.0;
-        /*v = Bezier.cubic( 0.0, arr );
-        p.push( { x: v.x, y: v.y } );*/
-        p.push( p0 );
+        v = Bezier.cubic( 0.0, arr );
+        p.push( { x: v.x, y: v.y } );
         t += step;
         while( t < 1 ){
             v = Bezier.cubic( t, arr );
             p.push( { x: v.x, y: v.y } );
             t += step;
         }
-        /*v = Bezier.cubic( 1.0, arr );
-        p.push( { x: v.x, y: v.y } );*/
-        p.push( p3 );
+        v = Bezier.cubic( 1.0, arr );
+        p.push( { x: v.x, y: v.y } );
         return p;
     }
     public static inline function distance(     p0: { x: Float, y: Float }
