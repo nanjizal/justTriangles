11d79f0f4deba6df91f746acb88430dab83c62be
nanjizal <nitsuj@nanjizal.net>
2017-01-26 00:34:48 +0000

    WIP - removing need for Point class.

--- justTriangles/PathContext.hx
+++ justTriangles/PathContext.hx
@@ -4,7 +4,7 @@ import justTriangles.Point;
 import justTriangles.ShapePoints;
 enum LineType {
     TriangleJoinCurve; // arc- Default seems to work quite well, but WIP.
-    TriangleJoinStraight; // straight 
+    TriangleJoinStraight; // straight   
     Poly;         // polygons
     Curves;       // curves
     Round;
@@ -18,7 +18,7 @@ abstract DrawDirection( Bool ){
 }
 @:enum
 abstract PolySides( Int ) {
-    var triangle        = 3;
+    var triangle        = 3;  
     var quadrilateral   = 4;
     var square          = 4;
     var tetragon        = 4;
@@ -52,13 +52,14 @@ abstract PolySides( Int ) {
     var chiliagon       = 1000;
     var myriagon        = 10000;
 }
-class PathContext implements IPathContext {
+class PathContext {
     public static var circleSides: PolySides = hexacontagon;
     var dirty: Bool = true;
-    var p0: Point;
-    var pp: Array<Point>;
-    var ppp: Array<Array<Point>>;
-    var ppp_: Array<Array<Point>>;
+    var p0x: Float;
+    var p0y: Float;
+    var pp: Array<Float>;
+    var ppp: Array<Array<Float>>;
+    var ppp_: Array<Array<Float>>;
     var s: Float;
     var dw: Float;
     var tx: Float;
@@ -71,53 +72,67 @@ class PathContext implements IPathContext {
         s = 1/width_;
         tx = tx_;
         ty = ty_;
-        ppp = new Array<Array<Point>>();
+        ppp = new Array<Array<Float>>();
         moveTo( dw, dw );
     }
-    inline function pt( x: Float, y: Float ): Point {
+    inline function ptx( x: Float, y: Float ): Point {
         // default is between ±1
-        return { x: s*( x - dw + tx ), y: s*( y - dw + ty ) }
+        return s*( x + tx ) - 0.5;
+    }
+    inline function pty( x: Float, y: Float ): Point {
+        // default is between ±1
+        return s*( y + ty ) - 0.5; //
     }
     public function moveTo( x: Float, y: Float ): Void {
         dirty = true;
-        p0 = pt( x, y );
-        if( pp != null ) if( pp.length == 1 ) ppp.pop(); // remove moveTo that don't have another drawing command after.
+        p0x = ptx( x );
+        p0y = pty( y );
+        if( pp != null ) if( pp.length < 3 ) ppp.pop(); // remove moveTo that don't have another drawing command after.
         pp = new Array();
-        pp.push( p0 );
+        pp.push( p0x );
+        pp.push( p0y );
         ppp.push( pp );
     }
-    function moveToPoint( p0: Point ): Void {
+    function moveToPoint( p0x: Float, p0y: Float ): Void {
         dirty = true;
-        if( pp != null ) if( pp.length == 1 ) ppp.pop(); // remove moveTo that don't have another drawing command after.
+        if( pp != null ) if( pp.length < 3 ) ppp.pop(); // remove moveTo that don't have another drawing command after.
         pp = new Array();
-        pp.push( p0 );
+        pp.push( p0x );
+        pp.push( p0y );
         ppp.push( pp );
     }
     public function lineTo( x: Float, y: Float ): Void {
-        var p1: Point = pt( x, y );
-        pp.push( p1 );
-        p0 = p1;
+        var p1x = ptx( x );
+        var p1y = pty( y );
+        pp.push( p1x );
+        pp.push( p1y );
+        p0x = p1x;
+        p0y = p1y;
     }
     public function quadTo( x1: Float, y1: Float, x2: Float, y2: Float ): Void {
-        var p1: Point = pt( x1, y1 );
-        var p2: Point = pt( x2, y2 );
-        var pMore: Array<Point> = ShapePoints.quadCurve( p0, p1, p2 );
-        var plen = pp.length;
-        for( i in 1...( pMore.length ) ) pp[ plen++ ] = pMore[ i ];
-        p0 = p2;
+        var p1x = ptx( x1 );
+        var p1y = pty( y1 );
+        var p2x = ptx( x2 );
+        var p2y = pty( y2 );
+        ShapePoints.quadCurve( pp, p0x, p0y, p1x, p1y, p2x, p2y );
+        p0x = p2x;
+        p0y = p2y;
     }
     public function curveTo( x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float ): Void {
-        var p1: Point = pt( x1, y1 );
-        var p2: Point = pt( x2, y2 );
-        var p3: Point = pt( x3, y3 );
-        var pMore = ShapePoints.cubicCurve( p0, p1, p2, p3 );
-        var plen = pp.length;
-        for( i in 1...( pMore.length ) ) pp[ plen++ ] = pMore[ i ];
-        p0 = p3;
+        var p1x = ptx( x1 );
+        var p1y = pty( y1 );
+        var p2x = ptx( x2 );
+        var p2y = pty( y2 );
+        var p3x = ptx( x3 );
+        var p3y = pty( y3 );
+        ShapePoints.cubicCurve( pp, p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y );
+        p0x = p3x;
+        p0y = p3y;
     }
     public function rectangle( x: Float, y: Float, width: Float, height: Float ): Void {
-        var p1: Point = pt( x, y );
-        var pMore = ShapePoints.box( p1.x, p1.y, width*s, height*s );
+        var p1x = ptx( x );
+        var p1y = pty( y );
+        var pMore = ShapePoints.box( new Array<Float>(), p1x, p1y, width*s, height*s ); // TODO: not ideal rethink?
         moveToPoint( pMore[0] );
         for( p in pMore  ) pp.push( p );
     }
@@ -125,9 +140,10 @@ class PathContext implements IPathContext {
     public function arc_move( x: Float, y: Float, radius: Float, start: Float, dA: Float, ?direction: DrawDirection, ?sides: PolySides ):Void{
         if( direction == null ) direction = clockwise;
         if( sides == null ) sides = circleSides;
-        var p1: Point = pt( x, y );
+        var p1x = ptx( x );
+        var p1y = pty( y );
         if( direction == counterClockwise ) dA = -dA; //TODO: Check
-        var pMore = ShapePoints.arcPoints( p1, radius*s, start, dA, cast sides );
+        var pMore = ShapePoints.arcPoints( new Array<Float>(), p1x, p1y, radius*s, start, dA, cast sides );
         moveToPoint( pMore[0] );
         for( p in pMore  ) pp.push( p );
     }
@@ -135,12 +151,12 @@ class PathContext implements IPathContext {
     public function arc( x: Float, y: Float, radius: Float, start: Float, dA: Float, ?direction: DrawDirection, ?sides: PolySides ):Void{
         if( direction == null ) direction = clockwise;
         if( sides == null ) sides = circleSides;
-        var p1: Point = pt( x, y );
+        var p1x = ptx( x );
+        var p1y = pty( y );
         if( direction == counterClockwise ) dA = -dA; //TODO: Check
-        var pMore = ShapePoints.arcPoints( p1, radius*s, start, dA, cast sides );
-        for( p in pMore  ) pp.push( p );
+        ShapePoints.arcPoints( pp, p1x, p1y, radius*s, start, dA, cast sides );
     }
-    // TODO: issue with closing rounded rectangle and some polys with the last line - webgl specific?
+    // working
     public function roundedRectangle( dx: Float, dy: Float, width: Float, height: Float, radius: Float ): Void {
         var pi = Math.PI;
         var pi_2 = Math.PI/2;
@@ -177,70 +193,115 @@ class PathContext implements IPathContext {
     }
     // currently only use predefined sides to encourage use of PolySides names.
     public function regularPoly( sides: PolySides, x: Float, y: Float, radius: Float, ?rotation: Float = 0 ): Void {
-        var p1: Point = pt( x, y );
-        var pMore = ShapePoints.polyPoints( p1, radius*s, cast sides, rotation );
-        moveToPoint( pMore[0] );
+        var p1x = ptx( x );
+        var p1y = pty( y );
+        var pMore = ShapePoints.polyPoints( new Array<Float>(), p1x, p1y, radius*s, cast sides, rotation );
+        moveToPoint( pMore[0], pMore[1] );
         for( p in pMore  ) pp.push( p );
     }
     public function render( thick: Float, ?outline: Bool = true ){
         Draw.thick = thick;
-        if( dirty ) reverseEntries();
+          //if( dirty ) reverseEntries(); Not sure about this needs some thought.
         for( pp0 in ppp_ ){
             switch( lineType ){
                 // Currently best line drawing enum still under active development.
                 case TriangleJoinCurve:
                     var draw = new Draw();
-                    for( i in 0...pp0.length ){
-                       if( i%1 == 0 && i< pp0.length - 2) Draw.triangleJoin( id, draw, pp0[ i ], pp0[ i + 1 ], thick/800, true );
+                    var l = pp0.length;
+                    var tot = l - 4;
+                    var j: Int = 0;
+                    for( i in 0...tot ){
+                        if( (i & 1) == 0 ){
+                            if( (j & 1) == 0 ){
+                                Draw.triangleJoin( id, draw, pp0[ i ], pp0[ i + 1 ], pp0[ i + 2 ], pp0[ i + 3 ], thick/800, true );
+                            }
+                            j++;
+                        }
                     }
-                case TriangleJoinStraight:
-                    var draw = new Draw();
-                    for( i in 0...pp0.length ){
-                       if( i%1 == 0 && i< pp0.length - 2) Draw.triangleJoin( id, draw, pp0[ i ], pp0[ i + 1 ], thick/800, false );
-                    }
-                // Other alternates still keeping till have developed ideal solution.
-                case Poly:
-                    // fairly optimal but broken
-                    Draw.poly( id, outline, pp0 );
-                case Curves:
-                    // Not quite working on round rectangle but working well otherwise
-                    //Draw.isolatedSpecial( id, pp0[ 0 ], pp0[ 1 ], thick/800 );
-                    for( i in 0...pp0.length ) {
-                        if( i%1 == 0 && i< pp0.length - 2) Draw.quad( id, outline, pp0, i );
-                    }
-                case Round:
-                    // Pretty perfect but over drawing
-                    for( i in 0...pp0.length ){
-                       if( i%1 == 0 && i< pp0.length - 2) Draw.isolatedLine( id, pp0[ i ], pp0[ i + 1 ], thick/800, true );
-                    }
-                case Isolated:
-                    // similar to poly but more broken
-                    for( i in 0...pp0.length ){
-                       if( i%1 == 0 && i< pp0.length - 2) Draw.isolatedLine( id, pp0[ i ], pp0[ i + 1 ], thick/800, false );
-                    }
-                case Quad:
-                    // very broken
-                    for( i in 0...pp0.length ) {
-                        if( i%1 == 0 && i< pp0.length - 2) Draw.quad( id, outline, pp0, i );
-                    }
+                case _:
+                    //throw( 'currently not implemented' ); 
             }
         }
     }
-    inline function reverseEntries(){
-        var p: Array<Point>;
-        if( ppp_ == null ) ppp_ = new Array<Array<Point>>();
+    
+    
+    /*
+case TriangleJoinStraight:
+    var draw = new Draw();
+    var l = p.length;
+    for( i in 0...pp0.length ){
+       if( i%1 == 0 && i< pp0.length - 2) Draw.triangleJoin( id, draw, pp0[ i ], pp0[ i + 1 ], thick/800, false );
+    }
+    
+// Other alternates still keeping till have developed ideal solution.
+case Poly:
+    // fairly optimal but broken
+    Draw.poly( id, outline, pp0 );
+case Curves:
+    // Not quite working on round rectangle but working well otherwise
+    //Draw.isolatedSpecial( id, pp0[ 0 ], pp0[ 1 ], thick/800 );
+    for( i in 0...pp0.length ) {
+        if( i%1 == 0 && i< pp0.length - 2) Draw.quad( id, outline, pp0, i );
+    }
+case Round:
+    // Pretty perfect but over drawing
+    for( i in 0...pp0.length ){
+       if( i%1 == 0 && i< pp0.length - 2) Draw.isolatedLine( id, pp0[ i ], pp0[ i + 1 ], thick/800, true );
+    }
+case Isolated:
+    // similar to poly but more broken
+    for( i in 0...pp0.length ){
+       if( i%1 == 0 && i< pp0.length - 2) Draw.isolatedLine( id, pp0[ i ], pp0[ i + 1 ], thick/800, false );
+    }
+case Quad:
+    // very broken
+    for( i in 0...pp0.length ) {
+        if( i%1 == 0 && i< pp0.length - 2) Draw.quad( id, outline, pp0, i );
+    }
+    */
+    
+    
+    
+    
+    inline function reverseEntries(){  // problematic with new Array<Array<Float>>
+        var p: Array<Float>;
+        if( ppp_ == null ) ppp_ = new Array<Array<Float>>();
         var plen = ppp.length;
         var plen_ = ppp_.length;
-        var pp0: Array<Point> = ppp[0];
+        var pp0: Array<Float> = ppp[0];
         for( i in plen_...plen ){
             // only add ones new ones
             pp0 = ppp[ i ];
             p = pp0.copy(); // not sure why not allowed to do all this in one line?
-            p.reverse();
+            reversePoints( p );
             ppp_[ i ] = p;
         }
         dirty = false;
     }
+    
+    public static inline function reversePoints( p: Array<Float> ): Array<Float> {
+        var xl: Float;
+        var yl: Float;
+        var xr: Float;
+        var yr: Float;
+        var l = p.length;
+        var lm = p.length - 1;
+        var half = Std.int( p.length/2 );
+        for( i in 0...half ){
+            if( (i & 1) == 0 ){
+                xl = p[ i ];
+                yl = p[ i+1 ];
+                r = lm - i;
+                xr = p[ r - 1 ];
+                yr = p[ r ];
+                p[i] = y;
+                p[i+1] = x;
+                p[r-1] = y;
+                p[r] = x;
+            }
+        }
+    }
+    
     public function clear(){
         dirty = true;
         ppp_ = null;
