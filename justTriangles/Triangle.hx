70b93acf7d4572b9ee73458362d4aa23aa402830
nanjizal <nanjizal@users.noreply.github.com>
2017-08-17 21:16:56 +0100

    Add Color for each corner to allow gradients.

--- justTriangles/Triangle.hx
+++ justTriangles/Triangle.hx
@@ -7,6 +7,11 @@ class Triangle {
     };
     public var id: Int;
     public var colorID: Int;
+    public var windingAdjusted: Bool;
+    public var colorA: Int;
+    public var colorB: Int;
+    public var colorC: Int;
+    
     public var outline: Bool;
     public var depth: Float;
     public var ax: Float;
@@ -61,7 +66,8 @@ class Triangle {
                         ){
         id = id_;
         outline = outline_;
-        if( adjustWinding( A_, B_, C_ ) ){
+        var adjust = adjustWinding( A_, B_, C_ );
+        if( adjust ){
             ax = A_.x;
             ay = A_.y;
             bx = C_.x;
@@ -76,8 +82,12 @@ class Triangle {
             cx = C_.x;
             cy = C_.y;
         }
+        windingAdjusted = adjust;
         depth = depth_;
         colorID = colorID_;
+        colorA = colorID_;
+        colorB = colorID_;
+        colorC = colorID_;
     }
     // A B C, you can find the winding by computing the cross product (B - A) x (C - A)
     inline static function adjustWinding( A_: Point, B_: Point, C_: Point ): Bool{
@@ -105,4 +115,45 @@ class Triangle {
     function sign( n: Float ): Int {
         return Std.int( Math.abs( n )/n );
     }
+    // no bounds checking
+    inline function liteHit( px: Float, py: Float ): Bool {
+        var planeAB = ( ax - px )*( by - py ) - ( bx - px )*( ay - py );
+        var planeBC = ( bx - px )*( cy - py ) - ( cx - px )*( by - py );
+        var planeCA = ( cx - px )*( ay - py ) - ( ax - px )*( cy - py );
+        return sign( planeAB ) == sign( planeBC ) && sign( planeBC ) == sign( planeCA );
+    }
+    // draws Triangle with horizontal strips 1px high.
+    public function drawStrips( drawRect: Float->Float->Float->Float->Void ){
+        var xi: Int         = Math.floor( x );
+        var yi: Int         = Math.floor( y );
+        var righti: Int     = Math.ceil( right );
+        var bottomi: Int    = Math.ceil( bottom );
+        var sx: Int = 0;
+        var ex: Int = 0;
+        var sFound: Bool;
+        var eFound: Bool;
+        // need to adjust for negative values thought required.
+        for( y0 in yi...bottomi ){ // loop vertically
+            sFound = false; // could remove if swapped floor and ceil on boundaries?
+            eFound = false; // not needed perhaps just for safety at mo.
+            for( x0 in xi...righti ){
+                if( liteHit( x0, y0 ) ) { // start strip
+                    sx = x0;
+                    sFound = true;
+                    break;
+                }
+            }
+            if( sFound ){
+                for( x0 in sx...righti ){ // end strip
+                    if( !liteHit( x0, y0 ) ){
+                        ex = x0;
+                        eFound = true;
+                        break;
+                    }
+                }
+                if( eFound ) drawRect( sx, y0, ex - sx, 1 );
+            }
+        }
+    }
+    
 }
